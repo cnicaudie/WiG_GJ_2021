296640f9d5d162fa133ba9e0ef57094c894e855d
Charlotte <charlotte.nicaudie@insa-rennes.fr>
2021-09-10 18:44:36 +0200

    Added randomized obstacle spawning

--- WiG_GJ/Assets/Scripts/Ground/GroundTile.cs
+++ WiG_GJ/Assets/Scripts/Ground/GroundTile.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GroundTile : MonoBehaviour
 {
     private GroundSpawner m_groundSpawner;
+
+    [SerializeField] private Transform m_parentObstacle;
+    [SerializeField] private List<GameObject> m_obstacles;
+    [SerializeField] private List<Transform> m_obstacleSpawnPoints;
 
     // =================================
 
     private void Start()
     {
         m_groundSpawner = FindObjectOfType<GroundSpawner>();
+        SpawnObstacles();
     }
 
     private void OnTriggerExit(Collider other)
@@ -18,5 +24,22 @@ public class GroundTile : MonoBehaviour
             m_groundSpawner.SpawnGround();
             Destroy(gameObject, 2f);
         }
+    }
+
+    private void SpawnObstacles()
+    {
+        int obstacleNumber = Random.Range(1, 4);
+
+        for (int i = 0; i < obstacleNumber; i++)
+        {
+            int spawnIndex = Random.Range(0, m_obstacleSpawnPoints.Count);
+            int obstacleTypeIndex = Random.Range(0, m_obstacles.Count);
+            
+            Transform spawnPoint = m_obstacleSpawnPoints[spawnIndex];
+
+            GameObject obstacle = m_obstacles[obstacleTypeIndex];
+
+            Instantiate(obstacle, spawnPoint.position, Quaternion.identity, m_parentObstacle);
+        } 
     }
 }
