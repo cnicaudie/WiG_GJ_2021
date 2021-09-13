675229548ff065a4bd2baca045d33e480c511610
Charlotte <charlotte.nicaudie@insa-rennes.fr>
2021-09-13 10:16:02 +0200

    Made the enemy float when trapped in bubble

--- WiG_GJ/Assets/Scripts/Enemies/Enemy.cs
+++ WiG_GJ/Assets/Scripts/Enemies/Enemy.cs
@@ -13,16 +13,27 @@ public class Enemy : MonoBehaviour
         get { return m_isTrappedInBubble; }
     }
 
+    private Vector3 m_floatingPosition;
+    private float m_yStartPosition;
+    private float m_floatAmplitude = 1f;
+    private float m_floatSpeed = 3f;
+
     // =================================
 
     private void Start()
     {
+        m_floatingPosition = transform.position;
+        m_yStartPosition = transform.position.y + m_floatAmplitude;
+
         m_bubbleTrap.gameObject.SetActive(false);
     }
 
-    public int GetCurrentColorIndex()
+    private void Update()
     {
-        return m_bubbleTrap.CurrentColorIndex;
+        if (m_isTrappedInBubble)
+        {
+            Float();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -30,10 +41,19 @@ public class Enemy : MonoBehaviour
         if (!m_isTrappedInBubble && collision.gameObject.GetComponent<Bullet>())
         {
             m_isTrappedInBubble = true;
-
-            // TODO : make the enemy float ?
             m_bubbleTrap.gameObject.SetActive(true);
             m_bubbleTrap.DefineRandomColor();
         }
+    }
+
+    public int GetCurrentColorIndex()
+    {
+        return m_bubbleTrap.CurrentColorIndex;
+    }
+
+    private void Float()
+    {
+        m_floatingPosition.y = m_yStartPosition + m_floatAmplitude * Mathf.Sin(m_floatSpeed * Time.time);
+        transform.position = m_floatingPosition;
     }
 }
